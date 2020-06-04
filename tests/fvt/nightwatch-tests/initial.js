577816366286b6658fe9c802594ed98f99205d99
michael zhou <michaelzhou09@hotmail.com>
2020-06-04 05:31:05 +0000

    COMPX341-20 A4 VERSION 1

--- tests/fvt/nightwatch-tests/initial.js
+++ tests/fvt/nightwatch-tests/initial.js
@@ -10,29 +10,29 @@ module.exports = {
         .end(done);
     },
 
-    'Navigate to the DemoDOI - valid zip': async (browser) => {
+    'Navigate to the DemoDOI - valid city name': async (browser) => {
         const demodoi = browser.page.demodoi();
         const { cityName } = demodoi.section;
     
         await demodoi.navigate().waitForElementVisible('@inputText');
 
         await demodoi.setValue('@inputText', [
-            '78641',
+            'Auckland',
             browser.Keys.ENTER
           ]);
     
         await demodoi.waitForElementVisible('@table');
 
-        cityName.expect.element('@firstApp').text.to.equal('Leander');
+        cityName.expect.element('@firstApp').text.to.equal('Auckland');
     },
 
-    'Navigate to the DemoDOI - invalid zip': async (browser) => {
+    'Navigate to the DemoDOI - invalid city name': async (browser) => {
         const demodoi = browser.page.demodoi();
     
         await demodoi.navigate().waitForElementVisible('@inputText');
 
         await demodoi.setValue('@inputText', [
-            '90000',
+            'initial',
             browser.Keys.ENTER
           ]);
     
@@ -41,18 +41,18 @@ module.exports = {
         demodoi.expect.element('@cityNotFound').text.to.equal('city not found');
     },
 
-    'Navigate to the DemoDOI - invalid input': async (browser) => {
+    'Navigate to the DemoDOI - invalid city name': async (browser) => {
         const demodoi = browser.page.demodoi();
     
         await demodoi.navigate().waitForElementVisible('@inputText');
 
         await demodoi.setValue('@inputText', [
-            'ABCDE',
+            '1234',
             browser.Keys.ENTER
           ]);
     
         await demodoi.waitForElementNotPresent('@table');
 
-        demodoi.expect.element('@invalidCity').text.to.equal('* should be a 5 digit number only');
+        demodoi.expect.element('@invalidCity').text.to.equal('* should be a NZ city name');
     },
 };
